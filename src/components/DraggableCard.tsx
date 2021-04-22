465d815e8867512ab52746d3a82afefc2b8eba67
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-04-22 15:22:11 +0900

    fix: type

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
 import useDrag from "../lib/useDrag";
 
-const DraggableCard = ({ children }: any) => {
+type Props = {
+	children: React.ReactChild;
+};
+const DraggableCard = ({ children }: Props) => {
 	const cardRef = useRef(null);
 	useDrag(cardRef);
 
